a3caf74cc02838c2e61f1a521d755a282921253b
Krishna <krishnastm112004@gmail.com>
2026-08-03 14:57:20 +0530

    key value

--- JavaScript/Array/keyvalue.js
+++ JavaScript/Array/keyvalue.js
@@ -1,12 +1,23 @@
 //
 
-const arr = ["274", "akf", "adk", 5, 8, 2];
-const [num1, ...num] = arr;
-console.log(num);
-console.log(num1);
+// const arr = ["274", "akf", "adk", 5, 8, 2];
+// const [num1, ...num] = arr;
+// console.log(num);
+// console.log(num1);
 
-console.log(typeof(num));
-console.log(typeof(num1));
+// console.log(typeof(num));
+// console.log(typeof(num1));
+
+
+const arr = ["sfsd", "gfg", [7, 2, 4, 6, [444, 555], 55, true, 4, [56, 89, 79], [983, "333"]]];
+console.log(arr.flat(2));
+
+
+// const[...data] = arr;
+// console.log(data);
+
+
+
 
 
 
